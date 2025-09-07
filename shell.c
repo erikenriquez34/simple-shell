417ae8b307e039a429812e5a6855b19ecf0fdc05
Erik Enriquez <erikenriquez34@gmail.com>
2025-09-07 15:56:14 -0400

    Process bg start and shell options

--- shell.c
+++ shell.c
@@ -6,14 +6,37 @@
 
 /* constants */
 const char prompt[16] = "erik@15t-dy100";
-const int MAXLINE = 1024;
-const int verbose = 1;
+#define MAXLINE 1024
+#define MAXJOBS 16
+#define MAXJID 1<<16;
+int verbose = 1;
+
+struct job_t {              /* The job struct */
+	pid_t pid;              /* job PID */
+	int jid;                /* job ID [1, 2, ...] */
+	int state;              /* UNDEF, BG, FG, or ST */
+	char cmdline[MAXLINE];  /* command line */
+};
+struct job_t jobs[MAXJOBS]; /* The job list */
+
+/* function prototypes */
+int builtin_cmd(char** argv);
+void eval(char* cmdline);
+void usage(void);
+
+int builtin_cmd(char** argv) {
+    if (strcmp(argv[0], "quit") == 0 || strcmp(argv[0], "exit") == 0) {
+        exit(0);
+    }
+	return 0;
+}
 
 /* evaluate a command line */
 void eval(char* cmdline) {
     char* argv[MAXLINE];
     char buf[MAXLINE];
     pid_t pid;
+    int bg;
 
     strcpy(buf, cmdline);
 
@@ -29,12 +52,16 @@ void eval(char* cmdline) {
     argv[argc] = NULL;
     if (argv[0] == NULL) {return;}
 
-    /* builtin exit */
-    if (strcmp(argv[0], "quit") == 0 || strcmp(argv[0], "exit") == 0) {
-        exit(0);
+    /* check for background */
+    if (argc > 0 && strcmp(argv[argc-1], "&") == 0) {
+        bg = 1;
+        argv[argc-1] = NULL;
     }
 
-    /* use exec to run command */
+    /* builtin commands */
+    builtin_cmd(argv);
+
+    /* if forked process, use exec to run command. Parent proceed to bg flag check */
     if ((pid = fork()) == 0) {
         if (execvp(argv[0], argv) < 0) {
             printf("%s: Command not found.\n", argv[0]);
@@ -42,13 +69,33 @@ void eval(char* cmdline) {
         }
     }
 
-    /* make parent wait for any fg */
-    waitpid(pid, NULL, 0);
+    /* use bg to either wait or run in background*/
+    if (!bg) {
+        waitpid(pid, NULL, 0);
+    } else {
+        printf("[%d] %s\n", pid, cmdline);
+    }
 }
 
 /* main loop */
 int main(int argc, char** argv) {
     char cmdline[MAXLINE];
+    char c;
+
+    while ((c = getopt(argc, argv, "hv")) != EOF) {
+		switch (c) {
+			case 'h': /* help */
+				usage();
+				break;
+
+			case 'v': /* verbose mode */
+				verbose = 1;
+				break;
+
+			default:
+				usage();
+		}
+	}
 
     while (1) {
         printf("\e[0;36m%s\e[0m> ", prompt);
@@ -64,4 +111,11 @@ int main(int argc, char** argv) {
         cmdline[strcspn(cmdline, "\n")] = '\0';
         eval(cmdline);
     }
+}
+
+void usage(void) {
+    printf("Usage: shell [-hv]\n");
+	printf("   -h   print this message\n");
+	printf("   -v   print additional diagnostic information\n");
+	exit(1);
 }
